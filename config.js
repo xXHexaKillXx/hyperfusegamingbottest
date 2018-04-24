14f0ac231b2a2e4ef9a4824de3deb00ffdebaf07
xXHexaKillXx <hexaKillbot@gmail.com>
2018-04-24 12:44:59 +0800

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const config = {
   "support": ["369860125827006474"],
 
   // Your Bot's Token. Available on https://discordapp.com/developers/applications/me
-  "token": ["BOT_TOKEN"],
+  "token": [" "];
 
   // Default per-server settings. New guilds have these settings. 
 
